785769da588942cbe33f0893193d3cabd7861d6c
fengbo <fengbo.1118@bytedance.com>
2020-06-23 01:34:33 +0800

    var

--- basic.go
+++ basic.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
+func variableZeroValue(){
+	var a int
+	var s string
+	fmt.Printf("%d %q\n", a, s)
+}
+
+func variableInitialValue()  {
+	var a, b int = 3, 4
+	var s string = "abc"
+	fmt.Println(a, b, s)
+}
+
+func variableTypeDeduction()  {
+	var a, b, c, s = 3, 4, true, "def"
+	fmt.Println(a, b, c, s)
+}
+
+func variableShorter()  {
+	a, b, c, s := 3, 4, true, "def"
+	fmt.Println(a, b, c, s)
+}
+
 func main() {
 	fmt.Println("Hello world")
+	variableZeroValue()
+	variableInitialValue()
+	variableTypeDeduction()
+	variableShorter()
 }
