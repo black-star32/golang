73884619c04ecaf1c4cd80e931910b90df3d73a6
fengbo <fengbo.1118@bytedance.com>
2020-06-26 02:39:41 +0800

    package

---
+++ queue/entry/entry.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"golang/queue"
+)
+
+func main() {
+	q := queue.Queue{1}
+
+	q.Push(2)
+	q.Push(3)
+	fmt.Println(q.Pop())
+	fmt.Println(q.Pop())
+	fmt.Println(q.IsEmpyt())
+	fmt.Println(q.Pop())
+	fmt.Println(q.IsEmpyt())
+}
--- tree/entry/entry.go
+++ tree/entry/entry.go
@@ -5,6 +5,21 @@ import (
 	"golang/tree"
 )
 
+type myTreeNode struct {
+	node *tree.Node
+}
+
+func (myNode *myTreeNode) postOrder(){
+	if myNode == nil || myNode.node == nil{
+		return
+	}
+	left := myTreeNode{myNode.node.Left}
+	left.postOrder()
+	right := myTreeNode{myNode.node.Right}
+	right.postOrder()
+	myNode.node.Print()
+}
+
 func main() {
 	var root tree.Node
 	root = tree.Node{Value: 3}
@@ -41,5 +56,9 @@ func main() {
 	ppRoot.Print()
 
 	root.Traverse()
+	fmt.Println()
+	myRoot := myTreeNode{&root}
+	myRoot.postOrder()
+	fmt.Println()
 }
 
