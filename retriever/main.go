4d0320c12f2d0dcd00d713f9c1e339b95c7839ae
fengbo <fengbo.1118@bytedance.com>
2020-06-26 10:25:48 +0800

    interface

--- retriever/main.go
+++ retriever/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"golang/retriever/mock"
+	"golang/retriever/real"
+)
+
+type Retriever interface {
+	Get(url string) string
+}
+
+func download(r Retriever) string{
+	return r.Get("http://www.baidu.com")
+}
+
 func main() {
-	$END$
+	var r Retriever
+	r = mock.Retriever{Content: "this is a fake test"}
+	r = real.Retriever{}
+	fmt.Println(download(r))
+
+	//fmt.Println(download(mock.Retriever{Content: "this is a fake test too"}))
 }
